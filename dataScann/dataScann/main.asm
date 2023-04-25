5f3627e9334587606432ad1305513aad46082d54
ironic833 <39494199+ironic833@users.noreply.github.com>
2023-04-25 10:39:37 +0100

    Added dataScan and array work practice code

---
+++ dataScann/dataScann/main.asm
@@ -0,0 +1,30 @@
+.386
+.model	flat
+
+.data	
+intArray SWORD	0,0,0,0,1,4,-57,46,7,8
+
+.code	
+main	proc
+	mov ebx,OFFSET intArray
+	mov ecx,LENGTHOF intArray
+
+L1:
+	cmp WORD PTR[ebx],0
+	jnz found
+	add ebx,2
+	loop L1
+	jmp notFound
+
+found:
+	movsx eax,WORD PTR[ebx]
+	jmp quit
+
+notFound:
+	mov eax,999999
+
+quit:
+	ret
+
+main endp
+end main
